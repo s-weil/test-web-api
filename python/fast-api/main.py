75a665db18cf7ebb38750578ceb299553d706892
snusnusnu <steffen.weil.ch@gmail.com>
2022-12-09 17:03:56 +0100

    add delayed for fastapi

--- python/fast-api/main.py
+++ python/fast-api/main.py
@@ -1,23 +1,23 @@
 ## pip install fastapi
 ## pip install "uvicorn[standard]"
 
-
 # run it: uvicorn main:app --reload
 
 from typing import Union
-
 from fastapi import FastAPI
+import time
 
 app = FastAPI()
 
 @app.get("/health")
-async def read_root():
+async def health():
     return {}
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
+@app.get("/delayed/{sleep_ms}")
+async def delayed(sleep_ms: int):
+    # TODO: randomize
+    time.sleep(sleep_ms)
+    return {}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
--- rust/actix-api/src/main.rs
+++ rust/actix-api/src/main.rs
@@ -1,15 +1,15 @@
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::{thread, time};
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer};
+use std::{ops::Deref, thread, time};
 
 #[get("/ping")]
 async fn ping(_: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().body("pong")
 }
 
-#[get("/delayed")]
-async fn delayed(_: HttpRequest) -> HttpResponse {
+#[get("/delayed/{time_ms}")]
+async fn delayed(_: HttpRequest, time_ms: web::Path<u64>) -> HttpResponse {
     // TODO: randomize it; use async delay
-    let ten_millis = time::Duration::from_millis(10);
+    let ten_millis = time::Duration::from_millis(*time_ms.as_ref());
     thread::sleep(ten_millis);
     HttpResponse::Ok().body("pong")
 }
